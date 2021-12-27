105536a8a61836f0f2402b42acf83070bef380c1
fatin natasha lokman <45757536+natashaaa@users.noreply.github.com>
2021-12-27 14:23:15 +0800

    Update GPA.js

--- screens/calculatorPage/GPA.js
+++ screens/calculatorPage/GPA.js
@@ -1,225 +1,342 @@
-import React, {useState, Component} from 'react';
-import { render } from 'react-dom';
+import React, {useState, useContext} from 'react';
 import RNPickerSelect from 'react-native-picker-select';
+import Apikey from "../../database/apiKey";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import { AuthContext } from "../../routes/authProvider";
+import Header from '../../shared/header';
+import { Icon } from 'react-native-elements';
 import { StyleSheet, 
     Text, 
     View, 
-    Modal,
-    Button,
-    TextInput,
-    Picker,
+    LogBox,
+    ScrollView,
+    Dimensions,
+    TouchableOpacity,
+    
   } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FlatList } from 'react-native-gesture-handler';
+  
+if (!firebase.apps.length) { firebase.initializeApp(Apikey.firebaseConfig); }
+
+const db = firebase.firestore();
+LogBox.ignoreLogs(['Setting a timer for a long period of time'])
 
   export default function GPA(){
+    const { user, Logout, profile } = useContext(AuthContext);
 
-    const [modalOpen,  setModalOpenz] = useState(false);
+    const listTab =[
+      {
+        Semester: 1
+      },
+      {
+        Semester: 2
+      },
+      {
+        Semester: 3
+      },
+      {
+        Semester: 4
+      },
+      {
+        Semester: 5
+      },
+      {
+        Semester: 6
+      },
+      {
+        Semester: 7
+      },
+    
+    ]
 
-    const [selectedValue, setSelectedValue] = useState("semester 1")
+    const data = [
+      {
+        Semester: 1,
+        CourseCode: "GIG1001",
+        Subject: "THE ISLAMIC AND ASIAN CIVILIZATION",
+        Grades: "A",
+        CreditHours: 2,
+        GradePoints: 8
+      },
+      {
+        Semester: 1,
+        CourseCode: "WIA1001",
+        Subject: "INFORMATION SYSTEMS",
+        Grades: "A-",
+        CreditHours: 3,
+        GradePoints: 11.1
+      },
+      {
+        Semester: 1,
+        CourseCode: "WIX1001",
+        Subject: "COMPUTING MATHEMATICS I",
+        Grades: "A-",
+        CreditHours: 3,
+        GradePoints: 11.1
+      },
+      {
+        Semester: 1,
+        CourseCode: "WIX1002",
+        Subject: "FUNDAMENTALS OF PROGRAMMING",
+        Grades: "A-",
+        CreditHours: 5,
+        GradePoints: 18.5
+      },
+      {
+        Semester: 1,
+        CourseCode: "WIX1003",
+        Subject: "COMPUTER SYSTEMS AND ORGANIZATION",
+        Grades: "A-",
+        CreditHours: 3,
+        GradePoints: 11.1
+      },
+
+      {
+        Semester: 2,
+        CourseCode: "GIG1002",
+        Subject: "ETHNIC RELATIONS",
+        Grades: "A",
+        CreditHours: 2,
+        GradePoints: 8
+      },
+      {
+        Semester: 2,
+        CourseCode: "GIG1005",
+        Subject: "SOCIAL ENGAGEMENT",
+        Grades: "A",
+        CreditHours: 2,
+        GradePoints: 8
+      },
+      {
+        Semester: 2,
+        CourseCode: "WIA1002",
+        Subject: "DATA STRUCTURE",
+        Grades: "C-",
+        CreditHours: 5,
+        GradePoints: 8.5
+      },
+      {
+        Semester: 2,
+        CourseCode: "WIA1003",
+        Subject: "COMPUTER SYSTEM ARCHITECTURE",
+        Grades: "C",
+        CreditHours: 3,
+        GradePoints: 6,
+      },
+      {
+        Semester: 2,
+        CourseCode: "WIA1004",
+        Subject: "FUNDAMENTALS OF ARTIFICIAL INTELLIGENCE",
+        Grades: "B+",
+        CreditHours: 3,
+        GradePoints: 9.9
+      },
+      {
+        Semester: 2,
+        CourseCode: "WIA1005",
+        Subject: "NETWORK TECHNOLOGY FOUNDATION",
+        Grades: "B-",
+        CreditHours: 4,
+        GradePoints: 10.8
+      },
+      {
+        Semester: 3,
+        CourseCode: "GIG1003",
+        Subject: "BASIC ENTREPRENEURSHIP CULTURE",
+        Grades: "A+",
+        CreditHours: 2,
+        GradePoints: 8.0
+      },
+      {
+        Semester: 3,
+        CourseCode: "GKP1001",
+        Subject: "TALENT DEVELOPMENT",
+        Grades: "A+",
+        CreditHours: 2,
+        GradePoints: 8.0
+      },
+      {
+        Semester: 3,
+        CourseCode: "WIA2001",
+        Subject: "DATABASE",
+        Grades: "A-",
+        CreditHours: 3,
+        GradePoints: 11.10
+      },
+      {
+        Semester: 3,
+        CourseCode: "WIA2002",
+        Subject: "SOFTWARE MODELLING",
+        Grades: "A-",
+        CreditHours: 3,
+        GradePoints: 11.10
+      },
+      {
+        Semester: 3,
+        CourseCode: "WIA2003",
+        Subject: "PROBABILITY AND STATISTIC",
+        Grades: "B",
+        CreditHours: 3,
+        GradePoints: 9.0
+      },
+      {
+        Semester: 3,
+        CourseCode: "WIC2003",
+        Subject: "DIGITAL DESIGN AND HARDWARE DESCRIPTION LANGUAGE",
+        Grades: "A+",
+        CreditHours: 3,
+        GradePoints: 12.0
+      },
+      {
+        Semester: 3,
+        CourseCode: "WIX2001",
+        Subject: "THINKING AND COMMUNICATION SKILLS",
+        Grades: "A",
+        CreditHours: 3,
+        GradePoints: 12.0
+      },
+      {
+        Semester: 3,
+        CourseCode: "WIX2002",
+        Subject: "PROJECT MANAGEMENT",
+        Grades: "A-",
+        CreditHours: 3,
+        GradePoints: 11.10
+      },
+
+    ]
+
+  const [semester, setSemester] = useState('All');
+  const [dataList, setDataList] = useState(data)
+
+  const setStatusFilter = Semester => {
+   
+    setDataList([...data.filter(e => e.Semester === Semester)])
+ 
+    setSemester(Semester)
+  };
+
+  const renderItem = ({item, index}) => {
+    return (
+      <ScrollView>
+      <View key={index} style={style.itemContainer}>
+        <View style={style.subjectBody}>
+          
+          <Text style={style.subjectFont}>{item.Subject}</Text>
+          <Text style={style.gradeFont}> {item.Grades}</Text>
+          
+        </View>
+
+      </View>
+      </ScrollView>
+    )
+    
+  }
 
       return(
-          <View style={{flex: 1, marginTop: 10}}>
-          
-            <View  style={style.infocontainer}>
-               <Button 
-               color="skyblue"
-               title=" List of grade and method to calculate"
-               onPress ={() =>  setModalOpenz (true)}
-               > 
-               </Button>
-            </View>
+    <SafeAreaView style={style.container}>
+    <Header text="GPA Result" />
+      <View style={style.resultcontainer}>
+        <Text style={style.resultfont}>
+          GPA result for selected semester:</Text>
+      </View>
 
-            <View style={style.containerCS}>
-              <Text style={style.fontCS}>Semesters:</Text>
-                <View style={style.containerCS2}>
-                  <Picker
-                    selectedValue={selectedValue}
-                    style={style.dropdowncontainer}
-                    onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-                  >
-                 
-                    <Picker.Item label="Semester 1" value="semester 1" />
-                    <Picker.Item label="Semester 2" value="semester 2" />
-                    <Picker.Item label="Semester 3" value="semester 3" />
-                    <Picker.Item label="Semester 4" value="semester 4" />
-                    <Picker.Item label="Semester 5" value="semester 5" />
-                    <Picker.Item label="Semester 6" value="semester 6" />
-                    <Picker.Item label="Semester 7" value="semester 7" />
-                  </Picker>
-                </View>
-            </View>
+      <ScrollView horizontal={true}
+                    showsHorizontalScrollIndicator={false}>
+        <View style={style.listTab}>
+          {
+            listTab.map(e =>(
+              <TouchableOpacity 
+              style={[style.btnTab, semester === e.Semester && style.btnTabActive]}
+              onPress={() => setStatusFilter(e.Semester)}
+              >
+                <Text style={style.textTab}>
+                  {e.Semester}</Text>
+              </TouchableOpacity>
+            ))
+          }
+        </View>
+      </ScrollView>
 
-            <View style={style.Calcontainer}>  
-                <Text style={{fontSize: 25, paddingLeft: 27, paddingTop: 15}}>4.00</Text>
-            </View> 
+      <FlatList
+      data = {dataList}
+      keyExtractor={(e, i) => i.toString()}
+      renderItem={renderItem}
+      />
+    </SafeAreaView>
 
-            <View style={style.Calbox}>
-                <View style={style.Calboxsubject} >
-                    <Text style={{fontSize: 15, paddingLeft: 27, paddingTop: 15}}>Course Code,   3 credit</Text>
-                </View>
+    )
+}
 
-                <View style={style.Calboxgrade} >
-                    <Text style={{fontSize: 15, paddingLeft: 15, paddingTop: 15}}> Grade</Text>
-                </View>
-            </View>
+const style = StyleSheet.create({
 
+  container:{
+    flex: 1,
+    marginBottom:30,
+  }, 
 
-            <Modal
-            transparent ={true}
-            visible={modalOpen}
-            >
-              <View style={{ backgroundColor:'#000000aa', flex: 1}}>
-                <View style={{backgroundColor:'white', margin:40, height:550, borderRadius: 10}}> 
+  listTab:{
+    flexDirection:'row',
+  },
 
-                <Text style={{ fontSize: 15, paddingTop: 20, paddingLeft: 20 }}>
-                  University of Malaya's grade and grade point list:
-                </Text>
-                <Text style={{ fontSize: 15, paddingTop: 20, paddingLeft: 30}}>
-                 A+      100-90       4.0
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 A         89-80         4.0
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 A-        79-75         3.7
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 B+       74-70         3.3
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 B         69-65         3.0
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 B-        64-60         2.7
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 C+       59-55         2.3
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 C         54-50         2.0
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 C-        49-45        1.7
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 D+       44-40        1.3
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30}}>
-                 D         39-35        1.0
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30, marginBottom: 20 }}>
-                 F         34-00         0.0
-                </Text>
+  btnTab:{
+    width: Dimensions.get('window').width/5.6,
+    flexDirection:'row',
+    borderWidth:1,
+    borderColor:'#EBEBEB',
+    padding:10,
+    borderRadius:20,
+    margin:5,
+    height:40,
+  },
 
-                <Text style={{ fontSize: 15, paddingTop: 20, paddingLeft: 20 }}>
-                Formula:
-                </Text>
-                <Text style={{ fontSize: 15, paddingLeft: 30, marginBottom: 50 }}>
-                GPA = Sum of grade point / Sum of credit hours 
-                </Text>
+  textTab:{
+    fontSize:15,
+    color:'black',
+    paddingLeft:20
+   
+  },
 
-           
-                
-                <View  style={style.closeinfocontainer}>
-                  <Button 
-                   color="skyblue"
-                   title="Close"
-                    onPress ={() =>  setModalOpenz (false)}
-                  > 
-                  </Button>
-                </View>
-                </View>
-              </View>
-           </Modal>
+  btnTabActive:{
+    backgroundColor:'steelblue'
+  },
 
-          </View>
-      )
-  }
+  itemContainer:{
+    paddingVertical:10,
+    flexDirection:'row',
+    alignSelf:'center',
+  },
+
+  subjectFont:{
+    fontSize:12,
+  },
   
-  const style = StyleSheet.create({
+  subjectBody:{
+    paddingHorizontal:15,
+    paddingVertical:15,
+    borderRadius:20,
+    borderColor:'steelblue',
+    borderWidth:0.5,
+    alignItems:'center',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    width: 350
+  },
 
-      dropdowncontainer: {
-        height: 34, 
-        width: 255
-      },
+  gradeFont:{
+  marginRight:10
+  },
 
-      infocontainer: {
-        width:370,
-        height: 40,
-        marginLeft:20
-      },
-      closeinfocontainer:{
-        width:200,
-        height: 40,
-        alignSelf: 'center',
-        
-      },
-
-      Calcontainer: {
-        backgroundColor: 'darkgrey',
-        width:100,
-        height: 60,
-        marginTop: 10,
-        borderRadius: 20,
-        alignSelf: 'center'
-      },
-
-      Calbutton:{
-        backgroundColor: 'lightblue',
-        width: 100,
-        borderRadius: 30,
-        marginLeft:24,
-        marginTop: 250,
-        height: 40,
-     },
-
-     Calbox:{
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',  
-        marginTop: 20,
-      },
-
-      Calboxsubject:{
-        height: 50, 
-        width: 280,
-        borderRadius: 20,
-        backgroundColor: 'powderblue'
-      },
-      
-      Calboxgrade:{
-        height: 50, 
-        width: 80,
-        borderRadius: 20,
-        marginLeft:10,
-        backgroundColor: 'skyblue'
-      },
-
-      containerCS:{
-        paddingLeft: 40,
-        flexDirection: 'row',
-      },
-
-      containerCS2:{
-        width: 260,
-        marginLeft: 10,
-        marginTop: 15,
-        borderRadius: 15,
-        height: 35,
-        borderWidth: 1,
-        borderColor: 'steelblue',
-        alignItems:'center'
-      },
-
-      fontCS:{
-        fontSize: 15,
-        paddingTop: 23,
-         
-      },
-
-      fontInput:{
-        paddingLeft: 20,
-        paddingTop:2
-        
-      },
-
-    })
+  resultfont:{
+    fontWeight:'bold',
+    fontSize:15,
+    paddingVertical:30,
+    paddingHorizontal:30,
+  },
+  resultcontainer:{
+    
+  },
+  
+  })
