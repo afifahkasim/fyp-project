26546284694df65336b11e89fc864cec21049cf1
fatin natasha lokman <45757536+natashaaa@users.noreply.github.com>
2021-12-30 20:27:41 +0800

    Update calculator.js

--- screens/calculatorPage/calculator.js
+++ screens/calculatorPage/calculator.js
@@ -1,4 +1,4 @@
-import React,{useState, useContext} from 'react';
+import React,{useState, useContext, useEffect} from 'react';
 import { Icon } from 'react-native-elements';
 import Apikey from "../../database/apiKey";
 import firebase from 'firebase/compat/app';
@@ -27,9 +27,368 @@ LogBox.ignoreLogs(['Setting a timer for a long period of time'])
 
   const { user, Logout, profile } = useContext(AuthContext);
 
-  const [modalOpen,  setModalOpenz] = useState(false);
+  const data = [
+    {
+      Semester: 1,
+      CourseCode: "GIG1001",
+      Subject: "THE ISLAMIC AND ASIAN CIVILIZATION",
+      Grades: "A-",
+      CreditHours: 2,
+      GradePoints: 7.40
+    },
+    {
+      Semester: 1,
+      CourseCode: "WIA1001",
+      Subject: "INFORMATION SYSTEMS",
+      Grades: "B+",
+      CreditHours: 3,
+      GradePoints: 9.90
+    },
+    {
+      Semester: 1,
+      CourseCode: "WIX1001",
+      Subject: "COMPUTING MATHEMATICS I",
+      Grades: "B",
+      CreditHours: 3,
+      GradePoints: 9.0
+    },
+    {
+      Semester: 1,
+      CourseCode: "WIX1002",
+      Subject: "FUNDAMENTALS OF PROGRAMMING",
+      Grades: "A-",
+      CreditHours: 5,
+      GradePoints: 18.5
+    },
+    {
+      Semester: 1,
+      CourseCode: "WIX1003",
+      Subject: "COMPUTER SYSTEMS AND ORGANIZATION",
+      Grades: "A-",
+      CreditHours: 3,
+      GradePoints: 11.1
+    },
 
-  const [target, setTarget] = useState(null);
+    {
+      Semester: 2,
+      CourseCode: "GIG1002",
+      Subject: "ETHNIC RELATIONS",
+      Grades: "B+",
+      CreditHours: 2,
+      GradePoints: 6.60
+    },
+    {
+      Semester: 2,
+      CourseCode: "GIG1005",
+      Subject: "SOCIAL ENGAGEMENT",
+      Grades: "A",
+      CreditHours: 2,
+      GradePoints: 8
+    },
+    {
+      Semester: 2,
+      CourseCode: "WIA1002",
+      Subject: "DATA STRUCTURE",
+      Grades: "C",
+      CreditHours: 5,
+      GradePoints: 10.0
+    },
+    {
+      Semester: 2,
+      CourseCode: "WIA1003",
+      Subject: "COMPUTER SYSTEM ARCHITECTURE",
+      Grades: "C+",
+      CreditHours: 3,
+      GradePoints: 6.90,
+    },
+    {
+      Semester: 2,
+      CourseCode: "WIA1004",
+      Subject: "FUNDAMENTALS OF ARTIFICIAL INTELLIGENCE",
+      Grades: "A",
+      CreditHours: 3,
+      GradePoints: 12.0
+    },
+    {
+      Semester: 2,
+      CourseCode: "WIA1005",
+      Subject: "NETWORK TECHNOLOGY FOUNDATION",
+      Grades: "A-",
+      CreditHours: 4,
+      GradePoints: 14.8
+    },
+    {
+      Semester: 3,
+      CourseCode: "GIG1003",
+      Subject: "BASIC ENTREPRENEURSHIP CULTURE",
+      Grades: "A+",
+      CreditHours: 2,
+      GradePoints: 8.0
+    },
+    {
+      Semester: 3,
+      CourseCode: "GKP1001",
+      Subject: "TALENT DEVELOPMENT",
+      Grades: "A+",
+      CreditHours: 2,
+      GradePoints: 8.0
+    },
+    {
+      Semester: 3,
+      CourseCode: "WIA2001",
+      Subject: "DATABASE",
+      Grades: "A-",
+      CreditHours: 3,
+      GradePoints: 11.10
+    },
+    {
+      Semester: 3,
+      CourseCode: "WIA2002",
+      Subject: "SOFTWARE MODELLING",
+      Grades: "A-",
+      CreditHours: 3,
+      GradePoints: 11.10
+    },
+    {
+      Semester: 3,
+      CourseCode: "WIA2003",
+      Subject: "PROBABILITY AND STATISTIC",
+      Grades: "B",
+      CreditHours: 3,
+      GradePoints: 9.0
+    },
+    {
+      Semester: 3,
+      CourseCode: "WIC2003",
+      Subject: "DIGITAL DESIGN AND HARDWARE DESCRIPTION LANGUAGE",
+      Grades: "A+",
+      CreditHours: 3,
+      GradePoints: 12.0
+    },
+    {
+      Semester: 3,
+      CourseCode: "WIX2001",
+      Subject: "THINKING AND COMMUNICATION SKILLS",
+      Grades: "A",
+      CreditHours: 3,
+      GradePoints: 12.0
+    },
+    {
+      Semester: 3,
+      CourseCode: "WIX2002",
+      Subject: "PROJECT MANAGEMENT",
+      Grades: "A-",
+      CreditHours: 3,
+      GradePoints: 11.10
+    },
+
+  ];
+
+  const current = [
+    {
+      Semester: 4,
+      CourseCode: "GIG1004",
+      Subject: "INFORMATION LITERACY",
+      Grades: null,
+      CreditHours: 2,
+      GradePoints: null
+    },
+    {
+      Semester: 4,
+      CourseCode: "GIS1007",
+      Subject: "INTRODUCTION TO POLICY AND MANAGEMENT OF SCIENCE AND TECHNOLOGY",
+      Grades: null,
+      CreditHours: 2,
+      GradePoints: null
+    },
+    {
+      Semester: 4,
+      CourseCode: "GLT1009",
+      Subject: "MASTERING ENGLISH VI",
+      Grades: null,
+      CreditHours: 3,
+      GradePoints: null
+    },
+    {
+      Semester: 4,
+      CourseCode: "WIA2004",
+      Subject: "OPERATING SYSTEMS",
+      Grades: null,
+      CreditHours: 4,
+      GradePoints: null
+    },
+    {
+      Semester: 4,
+      CourseCode: "WIA2005",
+      Subject: "ALGORITHM DESGIN AND ANALYSIS",
+      Grades: null,
+      CreditHours: 4,
+      GradePoints: null
+    },
+    {
+      Semester: 4,
+      CourseCode: "WIC2002",
+      Subject: "NETWORK SECURITY",
+      Grades: null,
+      CreditHours: 3,
+      GradePoints: null
+    },
+    {
+      Semester: 4,
+      CourseCode: "WIC2004",
+      Subject: "INTERNET TECHNOLOGY",
+      Grades: null,
+      CreditHours: 3,
+      GradePoints: null
+    },
+  ]
+
+//Semester 1
+const GradepointsSem1 = data.reduce((prev, current) => {
+ if ( current.Semester <= 1) prev += current.GradePoints;
+ return prev;}, 0);
+//console.log("GP sem1: ", GradepointsSem1);
+
+const CreditHoursSem1 = data.reduce((prev, current) => {
+  if (current.Semester <= 1) prev = prev + current.CreditHours;
+  return prev;}, 0);
+//console.log("CH sem1: ", CreditHoursSem1);
+
+const CGPAsem1 = (GradepointsSem1/ CreditHoursSem1).toFixed(2); 
+//console.log("cgpa sem1: ", CGPAsem1)
+
+
+//Semester 2
+const GradepointsSem2 = data.reduce((prev, current ) => {
+  if (current.Semester <= 2 ) prev = prev + current.GradePoints ;
+  return prev;}, 0);
+ //console.log("GP sem2: ", GradepointsSem2);
+
+ const CreditHoursSem2  = data.reduce(( prev, current) => {
+  if (current.Semester <= 2 )
+    prev = prev + current.CreditHours;
+  return prev ;}, 0);
+  //console.log("CH sem2: ", CreditHoursSem2);
+
+ const CGPAsem2 = (GradepointsSem2/ CreditHoursSem2).toFixed(2); 
+ // console.log("Cgpa sem2: ", CGPAsem2);
+
+
+ //Semester 3 
+const GradepointsSem3 = data.reduce((prev, current) => {
+  if (current.Semester <= 3) prev += current.GradePoints;
+  return prev;},  0);
+ //console.log("sem3", GradepointsSem3);
+
+ const CreditHoursSem3 =data.reduce((prev, current) => {
+  if (current.Semester <= 3) prev += current.CreditHours;
+  return prev;}, 0);
+ //console.log("CH sem3: ", CreditHoursSem3);
+
+ const CGPAsem3 = (GradepointsSem3/ CreditHoursSem3 ).toFixed(2); 
+ // console.log("Cgpa sem3: ", CGPAsem3);
+
+  //Semester 4
+const GradepointsSem4 = data.reduce((prev, current) => {
+  if (current.Semester <= 4) prev += current.GradePoints;
+  return prev;},  0);
+ const CreditHoursSem4 = data.reduce((prev, current) => {
+  if (current.Semester <= 4) prev += current.CreditHours;
+  return prev; }, 0);
+ const CGPAsem4 = (GradepointsSem4/CreditHoursSem4).toFixed(2); 
+
+   //Semester 5
+const GradepointsSem5 = data.reduce((prev, current) => {
+  if (current.Semester <= 5) prev += current.GradePoints;
+  return prev;}, 0);
+ const CreditHoursSem5 = data.reduce((prev, current) => {
+  if (current.Semester <= 5) prev += current.CreditHours;
+  return prev;}, 0);
+ const CGPAsem5 = (GradepointsSem5/ CreditHoursSem5).toFixed(2); 
+
+   //Semester 6
+const GradepointsSem6 = data.reduce((prev, current) => {
+  if (current.Semester <= 6) prev += current.GradePoints;
+  return prev;}, 0);
+
+ const CreditHoursSem6 = data.reduce((prev, current) => {
+  if (current.Semester <= 6) prev += current.CreditHours;
+  return prev;}, 0);
+
+ const CGPAsem6 = (GradepointsSem6/ CreditHoursSem6).toFixed(2); 
+
+   //Semester 7
+   const GradepointsSem7 = data.reduce((prev, current) => {
+    if (current.Semester <= 7) prev += current.GradePoints;
+    return prev;}, 0);
+  
+   const CreditHoursSem7 = data.reduce((prev, current) => {
+    if (current.Semester <= 7) prev += current.CreditHours;
+    return prev;}, 0);
+  
+   const CGPAsem7 = (GradepointsSem7/ CreditHoursSem7).toFixed(2); 
+
+
+//cgpa target
+
+//const maxSem = Math.max(...data.map(sem => sem.Semester));
+//const actualSem = maxSem +1;
+
+//cari total credit hour
+const PastCH = data.reduce((prev, current) => {prev += current.CreditHours;
+  return prev;}, 0);
+  console.log("pastCH:", PastCH);
+
+const CurrentCH = current.reduce((prev, current) => {prev += current.CreditHours;
+  return prev;}, 0);
+  console.log("currentCH:", CurrentCH);
+
+const totalCH =PastCH + CurrentCH;
+console.log("Total CH past + current:",totalCH);
+
+//cari current Grade point
+const [target, setTarget] = useState("");
+
+const CurrentGP = (CurrentCH * target); 
+console.log("CurrentGP:", CurrentGP);
+
+//grade point semester lepas
+const PastGP = data.reduce((prev, current) => {prev += current.GradePoints;
+  return prev;}, 0);
+  console.log("pastGP:", PastGP);
+
+const totalGP = (CurrentGP + PastGP).toFixed(2);
+console.log("totalGP:", totalGP);
+
+//cgpa target
+const CGPA = (totalGP/totalCH).toFixed(2);
+console.log("Target:", CGPA);
+
+
+//recommendation grade:
+const [message,setMessage] = useState("helllllllooo")
+
+  useEffect(() => {
+    let mm = target;
+
+    if(mm <= 3.0){
+      setMessage("Minimum 1 A in any sujects")
+    }
+    else if (3.0 < mm <= 3.5){
+      setMessage("Minimum 3 A in any subjects")
+    }
+    else if(3.5< mm <= 4.0){
+      setMessage("All A or Minimum 5 A in any subjects")
+    }
+    else{
+      setMessage(" ")
+    }
+  })
+
+
+
+
+const [modalOpen,  setModalOpenz] = useState(false);
 
   const GPAPressHandler = () => {  
     navigation.navigate('GPA');  
@@ -44,7 +403,7 @@ LogBox.ignoreLogs(['Setting a timer for a long period of time'])
       <ScrollView>
           <View  style={style.infocontainer}>
              <Button 
-             color="skyblue"
+             color="steelblue"
              title=" List of grade and method to calculate"
              onPress ={() =>  setModalOpenz (true)}
              > 
@@ -54,11 +413,12 @@ LogBox.ignoreLogs(['Setting a timer for a long period of time'])
           <Modal
           transparent ={true}
           visible={modalOpen}
+          animationType='fade'
           >
             <View style={{ backgroundColor:'#000000aa', flex: 1}}>
-              <View style={{backgroundColor:'white', margin:40, height:550, borderRadius: 10}}> 
+              <View style={{backgroundColor:'white', margin:40, height:600, borderRadius: 10}}> 
 
-              <Text style={{ fontSize: 15, paddingTop: 20, paddingLeft: 20 }}>
+              <Text style={{ fontSize: 15, paddingTop: 20, paddingLeft: 20, fontWeight:'bold' }}>
                 University of Malaya's grade and grade point list:
               </Text>
               <Text style={{ fontSize: 15, paddingTop: 20, paddingLeft: 30}}>
@@ -98,18 +458,19 @@ LogBox.ignoreLogs(['Setting a timer for a long period of time'])
                F         34-00         0.0
               </Text>
 
-              <Text style={{ fontSize: 15, paddingTop: 20, paddingLeft: 20 }}>
+              <Text style={{ fontSize: 15, paddingTop: 20, paddingLeft: 20,fontWeight:'bold', marginBottom: 20  }}>
               Formula:
               </Text>
-              <Text style={{ fontSize: 15, paddingLeft: 30, marginBottom: 50 }}>
-              GPA = Sum of grade point / Sum of credit hours 
+              <Text style={{ fontSize: 12, paddingLeft: 20, marginBottom: 20 }}>
+              GPA = Total of grade point / Total of credit hours 
               </Text>
-
-         
+              <Text style={{ fontSize: 12, paddingLeft: 20, marginBottom: 50 }}>
+              CGPA = Sum of Total grade point / Sum of total credit hours
+              </Text>
               
               <View  style={style.closeinfocontainer}>
                 <Button 
-                 color="skyblue"
+                 color="steelblue"
                  title="Close"
                   onPress ={() =>  setModalOpenz (false)}
                 > 
@@ -124,34 +485,46 @@ LogBox.ignoreLogs(['Setting a timer for a long period of time'])
                 <Text style={style.frontcaltext}>Semester Result</Text>
               </View>
           </Pressable>
-
+{/* 
           <Pressable onPress={CGPAPressHandler}>
               <View style={style.frontcalbutton}>
                 <Text style={style.frontcaltext}>GPA calculator</Text>
               </View>
           </Pressable>
-          
+*/}        
           </View>
 
           <View style={style.showCGPA}>
             <Text style={style.CGPAfont}>CGPA</Text>
-              <Text style={style.semesterfont}>Semester 1: </Text>
-              <Text style={style.semesterfont} >Semester 2: </Text>
-              <Text style={style.semesterfont}>Semester 3: </Text>
-              <Text style={style.semesterfont}>Semester 4: </Text>
-              <Text style={style.semesterfont}>Semester 5: </Text>
-              <Text style={style.semesterfont}>Semester 6: </Text>
-              <Text style={style.semesterfont}>Semester 7: </Text>
-            <Text style={style.targetfont}>GPA to achieve target:</Text>
+              <Text style={style.semesterfont}>Semester 1: {CGPAsem1}</Text>
+              <Text style={style.semesterfont}>Semester 2: {CGPAsem2}</Text>
+              <Text style={style.semesterfont}>Semester 3: {CGPAsem3}</Text>
+              <Text style={style.semesterfont}>Semester 4: {CGPAsem4}</Text>
+              <Text style={style.semesterfont}>Semester 5: {CGPAsem5}</Text>
+              <Text style={style.semesterfont}>Semester 6: {CGPAsem6}</Text>
+              <Text style={style.semesterfont}>Semester 7: {CGPAsem7} </Text>
+
+            <View style= {style.gpatargetcont}>
+            <Text style={style.targetfont}>GPA target: {target}</Text>
+            </View>
+
+            <View style= {style.cgpatargetcont}>
+            <Text style={style.targetfont}>Your CGPA: {CGPA}</Text>
+            </View>
+
+            <View style= {style.targetcont}>
+            <Text style={style.targetfont}>Recommendation grade: {message}</Text>
+            </View>
+
           </View>
 
           <View style={style.calculateTarget}>
             <View style={style.subcontainer}>
-              <Text style={style.calfont}>CGPA Target for this semester:</Text>
+              <Text style={style.calfont}>GPA Target for this semester:</Text>
                 <View>
                   <TextInput       
                         style={style.gradeinput}
-                        onChangeText={setTarget}
+                        onChangeText={(text) => setTarget(text)}
                         placeholder="4.00"
                         keyboardType="numeric"
                         value={target}/>   
@@ -192,47 +565,70 @@ LogBox.ignoreLogs(['Setting a timer for a long period of time'])
       height: 50,
       borderRadius: 20,
       alignSelf: 'center',
-      backgroundColor: 'white',
-      marginTop: 20,
-      borderWidth:1,
-      borderColor:'steelblue'
+      backgroundColor: 'steelblue',
+      marginTop: 20,  
     },
  
     frontcaltext:{
       textAlign: 'center',
       fontSize: 15,
-      paddingTop: 15
+      paddingTop: 15,
+      color:'white'
     },
 
     showCGPA:{
-      height: 330,
+      height:470,
       width:380,
-      backgroundColor:'white',
+      backgroundColor:'#fddfd4',
       alignSelf:'center',
       borderRadius:20,
       borderWidth:0.5,
-      borderColor:'steelblue',
+      borderColor:'#f37e50',
       marginTop: 30,
-
     },
 
     CGPAfont:{
       fontWeight:'bold',
       paddingHorizontal: 40,
-      paddingVertical:15,
-      fontSize:20
+      paddingTop:15,
+      fontSize:15,
+      color:'steelblue'
     },
 
     semesterfont:{
       paddingLeft:60,
-      paddingVertical:5
+      paddingVertical:5,
+      color:'steelblue'
     },
 
     targetfont:{
-      fontSize:20,
+      fontSize:15,
       fontStyle:'italic',
-      paddingLeft:50,
-      paddingVertical:20,
+      paddingLeft:30,
+      paddingVertical:15,
+      color:'white'
+    },
+
+    cgpatargetcont:{
+      marginHorizontal:100,
+      backgroundColor:'#f37e50',
+      borderRadius:20,
+      marginBottom:5,
+    },
+
+    gpatargetcont:{
+      marginHorizontal:100,
+      backgroundColor:'#f37e50',
+      borderRadius:20,
+      marginBottom:5,
+      marginTop:10,
+    },
+
+    targetcont:{
+      marginVertical:5,
+      marginHorizontal:20,
+      backgroundColor:'#f37e50',
+      borderRadius:20,
     },
 
     calculateTarget:{
