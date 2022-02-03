67a8dfaf7c56e191f48d8ca3c4698201c23ca88a
fatin natasha lokman <45757536+natashaaa@users.noreply.github.com>
2022-02-03 12:38:54 +0800

    update

--- screens/schedulerPage/courseScheduler.js
+++ screens/schedulerPage/courseScheduler.js
@@ -5,18 +5,20 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import { AuthContext } from "../../routes/authProvider";
 import Header from '../../shared/header';
+//import { CheckBox } from '@react-native-community/checkbox';
 import { StyleSheet, 
     Text, 
     View,
-    Button,
     Pressable,  
     TouchableOpacity,
     FlatList,
-    Dimensions,
     ScrollView,
-    LogBox
+    LogBox,
+    Alert,
+   
    
   } from 'react-native';
+import { CheckBox } from 'react-native-elements/dist/checkbox/CheckBox';
 
   if (!firebase.apps.length) { firebase.initializeApp(Apikey.firebaseConfig); }
 
@@ -30,16 +32,20 @@ import { StyleSheet,
     {
       id:1,
       Course_code: "GIG1005",
+      courseId: "GIG1005",
       Lecture_group: "-",
       Tutorial_group: "WIC",
-      Course_name: "THE ISLAMIC AND ASIAN CIVILIZATION",
+      Course_name: "The Islamic and Asian Civilization",
       Course_explanation: "heellloo",
       Credit_hour: 2,
       Lecturer_name: "PLY",
       Department: "Computer System and Network",
       Semester: 1,
       Year: "2020/2021",
-      Time: "Thursaday, 9-11am",
+      Time: "Thursday, 9-11am",
+      startTime: '9:00',
+      endTime: '11:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "49",
     },
@@ -47,16 +53,20 @@ import { StyleSheet,
     {
       id:2,
       Course_code: "GIG1005",
+      courseId: "GIG1005",
       Lecture_group: "-",
       Tutorial_group: "WID",
-      Course_name: "THE ISLAMIC AND ASIAN CIVILIZATION",
+      Course_name: "The Islamic and Asian Civilization",
       Course_explanation: "heellloo",
       Credit_hour: 2,
       Lecturer_name: "SSAR",
       Department: "Artificial Intelligence",
       Semester: 1,
       Year: "2020/2021",
-      Time: "Thursaday, 9-11am",
+      Time: "Thursday, 9-11am",
+      startTime: '9:00',
+      endTime: '11:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "91",
     },
@@ -64,16 +74,20 @@ import { StyleSheet,
     {
       id:3,
       Course_code: "GIG1005",
+      courseId: "GIG1005",
       Lecture_group: "-",
       Tutorial_group: "WIH",
-      Course_name: "THE ISLAMIC AND ASIAN CIVILIZATION",
+      Course_name: "The Islamic and Asian Civilization",
       Course_explanation: "heellloo",
       Credit_hour: 2,
       Lecturer_name: "AAN",
       Department: "Multimedia",
       Semester: 2,
       Year: "2020/2021",
-      Time: "Thursaday, 9-11am",
+      Time: "Thursday, 9-11am",
+      startTime: '9:00',
+      endTime: '11:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "49",
     },
@@ -81,16 +95,20 @@ import { StyleSheet,
     {
       id:4,
       Course_code: "GIG1005",
+      courseId: "GIG1005",
       Lecture_group: "-",
       Tutorial_group: "WIE",
-      Course_name: "THE ISLAMIC AND ASIAN CIVILIZATION",
+      Course_name: "The Islamic and Asian Civilization",
       Course_explanation: "heellloo",
       Credit_hour: 2,
       Lecturer_name: "AAN",
       Department: "Information Systems",
       Semester: 2,
       Year: "2020/2021",
-      Time: "Thursaday, 9-11am",
+      Time: "Thursday, 9-11am",
+      startTime: '9:00',
+      endTime: '11:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "44",
     },
@@ -98,16 +116,20 @@ import { StyleSheet,
     {
       id:5,
       Course_code: "GIG1005",
+      courseId: "GIG1005",
       Lecture_group: "-",
       Tutorial_group: "WIF",
-      Course_name: "THE ISLAMIC AND ASIAN CIVILIZATION",
+      Course_name: "The Islamic and Asian Civilization",
       Course_explanation: "heellloo",
       Credit_hour: 2,
       Lecturer_name: "NJ",
       Department: "Software Engineering",
       Semester: 2,
       Year: "2020/2021",
-      Time: "Thursaday, 9-11am",
+      Time: "Thursday, 9-11am",
+      startTime: '9:00',
+      endTime: '11:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "92",
     },
@@ -115,16 +137,20 @@ import { StyleSheet,
     {
       id:6,
       Course_code: "GIG1005",
+      courseId: "GIG1005",
       Lecture_group: "-",
       Tutorial_group: "WIG",
-      Course_name: "THE ISLAMIC AND ASIAN CIVILIZATION",
+      Course_name: "The Islamic and Asian Civilization",
       Course_explanation: "heellloo",
       Credit_hour: 2,
       Lecturer_name: "NF",
       Department: "Data Science",
       Semester: 2,
       Year: "2020/2021",
-      Time: "Thursaday, 9-11am",
+      Time: "Thursday, 9-11am",
+      startTime: '9:00',
+      endTime: '11:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "30",
     },
@@ -132,6 +158,7 @@ import { StyleSheet,
     {
       id:7,
       Course_code: "GIW1003",
+      courseId: "GIW1003",
       Lecture_group: "-",
       Tutorial_group: "K, T, L",
       Course_name: "Fun With Robots",
@@ -142,6 +169,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Monday, 3-5pm",
+      startTime: '15:00',
+      endTime: '17:00',
+      day: 1,
       Place: "Online",
       Number_Of_Student: "60",
     },
@@ -149,6 +179,7 @@ import { StyleSheet,
     {
       id:8,
       Course_code: "GIW1005",
+      courseId: "GIW1005",
       Lecture_group: "-",
       Tutorial_group: "K, T, L",
       Course_name: "Mobile Typography Made Easy",
@@ -159,6 +190,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Monday, 3-5pm",
+      startTime: '15:00',
+      endTime: '17:00',
+      day: 1,
       Place: "Online",
       Number_Of_Student: "30",
     },
@@ -166,6 +200,7 @@ import { StyleSheet,
     {
       id:9,
       Course_code: "GIW1006",
+      courseId: "GIW1006",
       Lecture_group: "-",
       Tutorial_group: "K, T, L",
       Course_name: "Mutimedia 360",
@@ -176,6 +211,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Monday, 3-5pm",
+      startTime: '15:00',
+      endTime: '17:00',
+      day: 1,
       Place: "Online",
       Number_Of_Student: "30",
     },
@@ -183,6 +221,7 @@ import { StyleSheet,
     {
       id:10,
       Course_code: "WIX1002",
+      courseId: "WIX1002",
       Lecture_group: "K",
       Tutorial_group: "-",
       Course_name: "Fundamentals Of Programming",
@@ -192,7 +231,10 @@ import { StyleSheet,
       Department: "All Department",
       Semester: 2,
       Year: "2020/2021",
-      Time: "Tuesday, 9-11pm",
+      Time: "Tuesday, 9-11am",
+      startTime: '9:00',
+      endTime: '11:00',
+      day: 2,
       Place: "Online",
       Number_Of_Student: "70",
     },
@@ -200,6 +242,7 @@ import { StyleSheet,
     {
       id:11,
       Course_code: "WIX1002",
+      courseId: "WIX1002",
       Lecture_group: "-",
       Tutorial_group: "T",
       Course_name: "Fundamentals Of Programming",
@@ -210,6 +253,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Thursday, 11-2pm",
+      startTime: '11:00',
+      endTime: '14:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "70",
     },
@@ -217,6 +263,7 @@ import { StyleSheet,
     {
       id:12,
       Course_code: "WIX1003",
+      courseId: "WIX1003",
       Lecture_group: "K",
       Tutorial_group: "-",
       Course_name: "Computer System and Organizations",
@@ -227,6 +274,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Tuesday, 3-6pm",
+      startTime: '13:00',
+      endTime: '18:00',
+      day: 2,
       Place: "Online",
       Number_Of_Student: "70",
     },
@@ -234,6 +284,7 @@ import { StyleSheet,
     {
       id:13,
       Course_code: "WIX2002",
+      courseId: "WIX2002",
       Lecture_group: "K",
       Tutorial_group: "-",
       Course_name: "Project Management",
@@ -244,6 +295,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Tuesday, 11-1pm",
+      startTime: '11:00',
+      endTime: '13:00',
+      day: 2,
       Place: "Online",
       Number_Of_Student: "30",
     },
@@ -251,6 +305,7 @@ import { StyleSheet,
     {
       id:14,
       Course_code: "WIX2002",
+      courseId: "WIX2002",
       Lecture_group: "K",
       Tutorial_group: "-",
       Course_name: "Project Management",
@@ -261,6 +316,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Thursday, 11-12pm",
+      startTime: '11:00',
+      endTime: '12:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "30",
     },
@@ -268,6 +326,7 @@ import { StyleSheet,
     {
       id:15,
       Course_code: "WIX3001",
+      courseId: "WIX3001",
       Lecture_group: "K",
       Tutorial_group: "-",
       Course_name: "Soft Computing",
@@ -278,6 +337,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Wednesday, 4-6pm",
+      startTime: '16:00',
+      endTime: '18:00',
+      day: 3,
       Place: "Online",
       Number_Of_Student: "240",
     },
@@ -285,6 +347,7 @@ import { StyleSheet,
     {
       id:16,
       Course_code: "WIX3001",
+      courseId: "WIX3001",
       Lecture_group: "-",
       Tutorial_group: "T1",
       Course_name: "Soft Computing",
@@ -295,6 +358,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Thursaday, 4-6pm",
+      startTime: '14:00',
+      endTime: '18:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "60",
     },
@@ -302,6 +368,7 @@ import { StyleSheet,
     {
       id:17,
       Course_code: "WIX3001",
+      courseId: "WIX3001",
       Lecture_group: "-",
       Tutorial_group: "T2",
       Course_name: "Soft Computing",
@@ -312,6 +379,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Friday, 2-3pm",
+      startTime: '14:00',
+      endTime: '15:00',
+      day: 5,
       Place: "Online",
       Number_Of_Student: "60",
     },
@@ -319,6 +389,7 @@ import { StyleSheet,
     {
       id:18,
       Course_code: "WIX3001",
+      courseId: "WIX3001",
       Lecture_group: "-",
       Tutorial_group: "T3",
       Course_name: "Soft Computing",
@@ -329,6 +400,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Thursaday, 1-2pm",
+      startTime: '13:00',
+      endTime: '114:00',
+      day: 4,
       Place: "Online",
       Number_Of_Student: "60",
     },
@@ -336,6 +410,7 @@ import { StyleSheet,
     {
       id:19,
       Course_code: "WIX3001",
+      courseId: "WIX3001",
       Lecture_group: "-",
       Tutorial_group: "T4",
       Course_name: "Soft Computing",
@@ -346,6 +421,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Friday, 11-12pm",
+      startTime: '11:00',
+      endTime: '12:00',
+      day: 5,
       Place: "Online",
       Number_Of_Student: "60",
     },
@@ -353,6 +431,7 @@ import { StyleSheet,
     {
       id:20,
       Course_code: "WIA1002 / WIB1002",
+      courseId: "WIA1002 / WIB1002",
       Lecture_group: "K1",
       Tutorial_group: "-",
       Course_name: "Data Structure",
@@ -363,6 +442,9 @@ import { StyleSheet,
       Semester: 2,
       Year: "2020/2021",
       Time: "Tuesday, 9-11am",
+      startTime: '9:00',
+      endTime: '11:00',
+      day: 2,
       Place: "Online",
       Number_Of_Student: "200",
     },
@@ -781,6 +863,7 @@ import { StyleSheet,
 
   const [department, setDepartment] = useState(null);
   const [dataList, setDepartmentList] = useState(courseData) 
+  const [data,setData] =useState(courseData)
 
   const setStatusFilter = Department => {
    
@@ -788,8 +871,25 @@ import { StyleSheet,
     setDepartment(Department)
   }
 
-  const renderItem = ({item, index}) => { 
+  const onChangeValue = (itemSelected, index) => {
+    const newData = data.map(item => {
+      if (item.id == itemSelected.id){
+        return{
+          ...item,
+          selected:!item.selected
+        }
+      }
+      return{
+        ...item,
+        selected: item.selected
+      }
+    })
+    setData(newData)
+  }
 
+  
+
+  const renderItem = ({item, index}) => { 
     return (
     <View style={{marginTop:5}}>
         <View  key={index}  style={{flex: 1}}>
@@ -801,43 +901,77 @@ import { StyleSheet,
         <Text style = {style.textCC1}>Lecturer: {item.Lecturer_name}</Text>
         <Text style = {style.textCC1}>Location: {item.Place}</Text>
         <View style = {style.addButton}>
+
+      {/*  <CheckBox
+              disabled={false}
+              onAnimationType='fill'
+              offAnimationType='fade'
+              boxType='square'
+              //checked={!item}
+              onPress={() => onChangeValue (item, index)}
+              /> */}  
+
           <Icon
             name='plus'
             type='evilicon'
             color='#517fa4' 
-            size={30}
-            onPress={() => console.log('hello')} 
-          />         
+            size={40}
+            onPress={() => onChangeValue (item, index)}
+           // onPress={() => navigation.navigate('Mycourse', {
+           //   paramKey: item.Course_name
+           // })} 
+          />       
       </View>
 
         <View
             style={{
               borderBottomColor: 'lightgrey',
               borderBottomWidth: 1,
-              marginTop: 5,
               width: 380,
               alignSelf: 'center',
+              marginTop:5,
             }}
         />
         </View>
     </View>
     )} 
 
+    const onPressShowItems =() =>{
+      const listSelected = data.filter(item => item.selected == true);
+      let contentAlert ='';
+      listSelected.forEach(item => {
+        contentAlert = contentAlert + "- " + item.Course_name + "\n"
+      })
+      Alert.alert(
+        "Courses",
+        contentAlert,
+        );
+    }
+
     return(
       <View style={{flex:1}}>
         <Header text="Course Scheduler"/>
 
+        <View style ={style.plannercont}>
         <Pressable onPress={pressPlanner}>
-              <View style={style.buttonMC}>
+              <View style={style.buttonSC}>
                 <Text style={style.buttonText}>Course Planner</Text>
               </View>
         </Pressable>
 
-        <TouchableOpacity
-          onPress= {pressHendlers}
-          style={style.buttonMC}>
-          <Text  style={style.buttonText}>My Course</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            //onPress= {pressHendlers}
+            onPress={onPressShowItems}
+            style={style.buttonSC}>
+            <Text  style={style.buttonText}>Selected Course</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress= {pressHendlers}
+            style={style.buttonSC}>
+            <Text  style={style.buttonText}>Timetable</Text>
+          </TouchableOpacity>
+        </View>
 
         <View
               style={{
@@ -846,11 +980,21 @@ import { StyleSheet,
                 marginTop: 5,
                 width: 380,
                 alignSelf: 'center',
-                marginBottom:10
+                marginBottom:15
                 }}
             />
 
-        <Text style={{paddingLeft:20}}>Choose Department:</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Icon
+              style={{paddingLeft: 20}}
+              name='building'
+              type='font-awesome-5'
+              color='steelblue' 
+              size={15}
+            />
+          <Text style={{paddingLeft: 10}} >Department:</Text>
+          
+        </View>    
 
         <ScrollView horizontal={true}
                         showsHorizontalScrollIndicator={false}>
@@ -873,7 +1017,6 @@ import { StyleSheet,
           keyExtractor={(e, i) => i.toString()}
           renderItem={renderItem}
           />
-
       </View>
    
     )
@@ -896,8 +1039,8 @@ import { StyleSheet,
 
     buttonText:{
       alignSelf: 'center',
-      paddingTop: 7,
-      fontSize: 15,
+      paddingTop: 9,
+      fontSize: 14,
       color:'white'
     },
 
@@ -919,26 +1062,32 @@ import { StyleSheet,
       marginRight: 25
       },
 
-    buttonMC:{
-      width: 340,
+    buttonSC:{
+      width: 120,
       height: 40,
       borderRadius: 20,
       alignSelf: 'center',
       backgroundColor: '#6F8FAF',
-      marginTop: 5, 
+      margin:5
+    },
+    plannercont:{
+      marginTop: 5,  
+      flexDirection:'row',
+      alignSelf:'center'
     },
 
     listTab:{
       flexDirection:'row',
-      marginBottom:17
+      marginBottom:20
     },
     
     btnTab:{
-      width: Dimensions.get('window').width/1.9,
+      //width: Dimensions.get('window').width/1.9,
+      paddingHorizontal:10,
       flexDirection:'row',
       borderWidth:1,
       borderColor:'#EBEBEB',
-      padding:10,
+      paddingVertical:10,
       borderRadius:20,
       margin:5,
       height:38, 
@@ -947,7 +1096,6 @@ import { StyleSheet,
     textTab:{
       fontSize:12,
       color:'black',
-      paddingLeft:10,
     },
     
     btnTabActive:{
